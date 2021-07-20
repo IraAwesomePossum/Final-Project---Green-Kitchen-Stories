71688a8d58bb67a7f700ad64f00a1ba9d65192a3
Irina <ifalko@protonmail.com>
2021-07-20 16:53:34 +0200

    first commit

---
+++ src/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./navbar.css";
+
+export default function Navbar() {
+  return (
+    <div className="nav">
+      <div className="topLeft">Left</div>
+      <div className="topCenter">center</div>
+      <div className="topRight">right</div>
+    </div>
+  );
+}
