586ea6dc0df8a3211553a98a76e87fa2a74a7a2e
Irina <ifalko@protonmail.com>
2021-07-23 12:54:23 +0200

    Routs added

--- src/App.js
+++ src/App.js
@@ -12,13 +12,27 @@ function App() {
   return (
     <Router>
       <Navbar />
-      {/* <Home /> */}
-      {/* <Single /> */}
-      {/* <Create /> */}
-      {/* <Settings/> */}
-      {/* <Login /> */}
-      {/* <Register /> */}
-      <Footer />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/create">
+          <Create />
+        </Route>
+        <Route path="/settings">
+          <Settings />
+        </Route>
+        <Route path="/post/:postId">
+          <Single />
+        </Route>
+      </Switch>
+      {/* <Footer /> */}
     </Router>
   );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,11 +2,13 @@ import "./footer.css";
 
 export default function Footer() {
   return (
-    <div className="footer">
-      <h5>
-        &copy; {new Date().getFullYear()}
-        <span> Green Kitchen Stories </span>All rights reserved
-      </h5>
+    <div className="wrapper">
+      <div className="footer">
+        <h5>
+          &copy; {new Date().getFullYear()}
+          <span> Green Kitchen Stories </span>All rights reserved
+        </h5>
+      </div>
     </div>
   );
 }
