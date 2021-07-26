e970e7dc9c710c8aaad44a3ccd63dadcce95b301
Irina <ifalko@protonmail.com>
2021-07-26 14:58:59 +0200

    user route added

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
 
 router.put("/:id", async (req, res) => {
@@ -9,9 +10,13 @@ router.put("/:id", async (req, res) => {
       req.body.password = await bcrypt.hash(req.body.password, salt);
     }
     try {
-      const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-        $set: req.body,
-      });
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
       res.status(200).json(updatedUser);
     } catch (err) {
       res.status(500).json(err);
@@ -19,6 +24,29 @@ router.put("/:id", async (req, res) => {
     }
   } else {
     res.status(401).json("You are not authorised");
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Your account was deactivated");
+    } catch (err) {
+      res.status(404).json("User not found");
+    }
+  } else {
+    res.status(401).json("You are not authorised");
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...info } = user._doc;
+    res.status(200).json(info);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
