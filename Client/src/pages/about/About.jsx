bf3a9256231882fc684e90ff2a9bbf075657c803
Irina <ifalko@protonmail.com>
2021-07-28 16:57:31 +0200

    About page added

--- Client/src/pages/about/About.jsx
+++ Client/src/pages/about/About.jsx
@@ -1,36 +1,30 @@
+import { useRef } from "react";
 import "./about.css";
 
 export default function About() {
   return (
-    <div className="about">
+    <>
       <img
         className="aboutImage"
-        src="https://images.pexels.com/photos/4261783/pexels-photo-4261783.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+        src="https://images.pexels.com/photos/6707376/pexels-photo-6707376.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
       />
       <div className="aboutText">
         <h1> About this blog </h1>
-        I'm baby swag cold-pressed jianbing banjo deep v. IPhone etsy viral,
-        farm-to-table keffiyeh umami banjo asymmetrical subway tile brunch
-        flexitarian shoreditch lomo fam leggings. Celiac activated charcoal poke
-        woke +1 meggings four loko photo booth keytar taiyaki glossier mustache
-        prism 90's health goth. Tbh umami vape, keytar fixie coloring book
-        vaporware iceland skateboard sriracha 90's intelligentsia craft beer
-        squid. Tofu selfies before they sold out knausgaard. Butcher cronut
-        bushwick, pug subway tile pork belly umami venmo jianbing try-hard fanny
-        pack bicycle rights williamsburg. Chillwave cray 90's, gluten-free
-        occupy bicycle rights farm-to-table keffiyeh bushwick. Chartreuse
-        locavore shoreditch chillwave actually thundercats polaroid. Chambray
-        shabby chic 8-bit echo park sustainable, celiac edison bulb schlitz VHS
-        YOLO pickled. Banh mi lyft cold-pressed ennui chillwave, pok pok
-        snackwave yr meggings fanny pack kogi quinoa DIY. Quinoa fashion axe
-        pitchfork echo park vice yuccie, sriracha live-edge tousled viral
-        iceland drinking vinegar lumbersexual kogi. Brunch listicle hoodie air
-        plant celiac four dollar toast. Lumbersexual pinterest letterpress,
-        marfa hoodie biodiesel lo-fi normcore pitchfork asymmetrical sustainable
-        humblebrag glossier vegan. Echo park viral lumbersexual fixie 90's. Woke
-        fingerstache chicharrones brooklyn, cray yuccie synth af raclette
-        gentrify quinoa kale chips chia pabst swag.
       </div>
-    </div>
+      <div className="desc">
+        <h3>MEET SARAH</h3>
+        <p>
+          Sarah Green lives in Berlin with her cat Voltaire. Sarah is a
+          25-year-old medical student who enjoys vandalising bus stops, eating
+          out and learning new languages. Sarah's best friend is a medical
+          student called Rae Khan. They get on well most of the time. She also
+          hangs around with Anita Bell and Christian Fox. They enjoy
+          binge-watching boxed sets together. In March 2021 she created this
+          blog to inspire people cooking plant-based recipes. If she is not
+          cooking, she is hiking, travelling or doing something else with her
+          life.
+        </p>
+      </div>
+    </>
   );
 }
